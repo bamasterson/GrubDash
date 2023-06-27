75f55c7c3a59d198584de446fbc60f004759d796
bamasterson <mastersonba@gmail.com>
2023-06-26 20:23:59 -0700

    C:/Program Files/Git/dishes route complete

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,61 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+//list function
+function list(req, res, next) {
+    res.json({ data: dishes });
+}
+
+function dishValidator(req, res, next) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    //validate all dish fields
+    const requiredFields = {
+        name,
+        description, 
+        price,
+        image_url,
+    };
+
+    for (let field in requiredFields) {
+        if (!requiredFields[field]) {
+            return next({ status: 400, message: `Dish must include a ${field}`});
+        }
+    }
+    //validate dish price
+    const dishPrice = requiredFields.price;
+
+    if (typeof dishPrice !== "number" || dishPrice <= 0) {
+        return next({
+            status: 400,
+            message: "Dish must have a price that is a number greater than 0"
+        });
+    }
+
+    next();
+}
+
+//create function
+function create(req, res, next) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+    };
+
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+
+module.exports = {
+    list,
+    create: [
+        dishValidator,
+        create
+    ],
+
+}
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
+const controller = require('./orders.controller');
+const methodNotAllowed = require('../errors/methodNotAllowed');
 
 // TODO: Implement the /orders routes needed to make the tests pass
+
+
+router.route('/:orderId')
+
+
+router.route('/')
+
+
 
 module.exports = router;
